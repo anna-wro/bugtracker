760c8a8181b55975d82974f0c7aa44a14f4026ac
anna-wro <wro.online@gmail.com>
2017-06-26 18:08:30 +0200

    📃 Disabled inputs style

--- app/web/js/main.js
+++ app/web/js/main.js
@@ -13,11 +13,21 @@ $('#project_type_start_date').dateDropper();
 $('#project_type_end_date').dateDropper();
 
 let projectDiv = $('#bug_type_expected_result').parent().next()[0];
-let typeDiv = projectDiv.nextElementSibling;
-let priorityDiv = typeDiv.nextElementSibling;
-let statusDiv = priorityDiv.nextElementSibling;
 
-projectDiv.classList.add('is-1-2', 'select-project');
-typeDiv.classList.add('is-1-2', 'select-type');
-priorityDiv.classList.add('is-2-3', 'select-priority');
-statusDiv.classList.add('is-1-3', 'select-status');
+if (projectDiv) {
+    let typeDiv = projectDiv.nextElementSibling;
+    let priorityDiv = typeDiv.nextElementSibling;
+    let statusDiv = priorityDiv.nextElementSibling;
+
+    projectDiv.classList.add('is-1-2', 'select-project');
+    typeDiv.classList.add('is-1-2', 'select-type');
+    priorityDiv.classList.add('is-2-3', 'select-priority');
+    statusDiv.classList.add('is-1-3', 'select-status');
+}
+
+let readonlyInputs = document.querySelectorAll('input[readonly="readonly"]');
+if (readonlyInputs) {
+    for (let input of readonlyInputs) {
+        input.disabled = true;
+    }
+}
